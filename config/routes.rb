aa1a80d05edd246a3a962e3dd47070fc140aebfc
zhaoyue <dourayme321@163.com>
2017-02-22 16:33:56 +0800

    add bootstrap html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  namespace :admin do
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
