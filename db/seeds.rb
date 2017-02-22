6d7902e1369ab491f97ac5c4cd5b269387c7cc0e
zhaoyue <dourayme321@163.com>
2017-02-22 17:36:24 +0800

    only amdin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.new
+u.email = "dourayme321@163.com"          
+
+u.password = "123456"                # 最少要六码
+
+u.password_confirmation = "123456"   # 最少要六码
+
+u.is_admin = true
+u.save
